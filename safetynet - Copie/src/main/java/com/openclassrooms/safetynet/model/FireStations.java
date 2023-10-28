80b995009b72943b8ed70a475cf153f7a8b7a17a
CedricRgx <134865690+CedricRgx@users.noreply.github.com>
2023-10-28 15:20:55 +0200

    Modification du README.md et bascule sur la branche de dev

---
+++ safetynet - Copie/src/main/java/com/openclassrooms/safetynet/model/FireStations.java
@@ -0,0 +1,32 @@
+package com.openclassrooms.safetynet.model;
+
+public class FireStations {
+
+    private int id;
+    private String adress;
+    private int station;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public int getStation() {
+        return station;
+    }
+
+    public void setStation(int station) {
+        this.station = station;
+    }
+}
